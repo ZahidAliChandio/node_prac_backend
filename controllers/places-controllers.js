9b6dafeeef17b5e0ab6a3573a33ab2de9861d5f4
ZahidAliChandio <zahidalichandio240@gmail.com>
2022-12-15 11:39:06 +0500

    usersRoutes done.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4, v4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const HttpError = require("../models/http-error");
 
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,27 +1,11 @@
 const express = require("express");
 
+const { getUsers, login, signup } = require("../controllers/users-controllers");
+
 const router = express.Router();
 
-const USERS = [
-  {
-    id: "u1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484472,
-      long: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 1001",
-  },
-];
-
-router.get("/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const user = USERS.find((u) => {
-    return u.id === userId;
-  });
-  console.log("User get request Called");
-  res.json({ user });
-});
+router.get("/", getUsers);
+router.post("/login", login);
+router.post("/signup", signup);
 
 module.exports = router;
