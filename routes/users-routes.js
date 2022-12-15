080784d56126b48d6e2b52d7b04fc4ea7017cbfa
ZahidAliChandio <zahidalichandio240@gmail.com>
2022-12-15 14:15:24 +0500

    user validation implemented.

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const { getUsers, login, signup } = require("../controllers/users-controllers");
 
@@ -6,6 +7,16 @@ const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/login", login);
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email")
+      .normalizeEmail() //Test@test.com =>(converted to) test@test.com
+      .isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  signup
+);
 
 module.exports = router;
