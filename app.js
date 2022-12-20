e49d3b2a2fe468cb4606fb2caee55c01f367cc02
ZahidAliChandio <zahidalichandio240@gmail.com>
2022-12-20 19:24:20 +0500

    place Schema created.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // app.js refers to server.js
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
+mongoose.set("strictQuery", false);
 const app = express();
 
 app.use(bodyParser.json());
@@ -28,4 +30,15 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured" });
 });
 
-app.listen(8000);
+mongoose
+  .connect(
+    "mongodb+srv://ZahidAli:ChandioAli11*@cluster0.cb7fetm.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("Connected to DB Successfully");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
